2be2ed579f5d75bcd1d4cbfe6c5d8d1f938bd320
ben_coelho <ben_coelho@hotmail.com>
2020-06-20 13:49:44 +1000

    more advanced query

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-
 var queryURL = "https://api.domain.com.au/v1/listings/residential/_search";
 
 $.ajax({
-    url: queryURL,
-    method: "POST",
-    headers: {accept: "application/json", "X-Api-Key": "key_761023226fcf3d4ecaa2f37b37d35d19"},
-    datatype: "json",
-    data: JSON.stringify({
-        "listingType":"Sale",
-        "propertyTypes":[
-          "House",
-          "NewApartments"
-        ],
-        "minBedrooms":3,
-        "minBathrooms":2,
-        "minCarspaces":1,
-        "locations":[
-          {
-            "state":"NSW",
-            "region":"",
-            "area":"",
-            "suburb":"Newtown",
-            "postCode":"",
-            "includeSurroundingSuburbs":false
-          }
-        ]
-      }),
-
-      error: function(e) {
-        console.log(e);
+  url: queryURL,
+  method: "POST",
+  headers: {
+    accept: "application/json",
+    "X-Api-Key": "key_761023226fcf3d4ecaa2f37b37d35d19",
+  },
+  datatype: "json",
+  data: JSON.stringify({
+    listingType: "Sale",
+    propertyTypes: ["House", "apartmentUnitFlat", "townhouse"],
+    minBedrooms: 3,
+    minBathrooms: 2,
+    minCarspaces: 1,
+    pageSize: 10,
+    locations: [
+      {
+        state: "VIC",
+        region: "",
+        area: "",
+        suburb: "Glen huntly",
+        postCode: "",
+        includeSurroundingSuburbs: false,
       },
-      contentType: "application/json"
+    ],
+  }),
 
-}).then(function(response) {
-    console.log(response);
-})
-
-
-
+  error: function (e) {
+    console.log(e);
+  },
+  contentType: "application/json",
+}).then(function (response) {
+  console.log(response);
+});
