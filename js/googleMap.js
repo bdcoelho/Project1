25cd94de5a340ad74b668ea8a36194be3551037b
shakib <shakib1122@gmail.com>
2020-06-27 14:39:54 +1000

    Google API final testing

--- js/googleMap.js
+++ js/googleMap.js
@@ -1,33 +1,45 @@
-$(document).ready(function() {
-    apiKey ="AIzaSyDkoaZdFxBFFsVXF2Hey8DXZ-tIiR_NBYM"
+function initMap () {
+    apiKey = "AIzaSyDkoaZdFxBFFsVXF2Hey8DXZ-tIiR_NBYM"
 
-    var workAddress = "436%20Johnston%20St,%20Abbotsford%20VIC%203067,au";
+    var workAddress = "436 Johnston st Abbotsford,au";
+
+    var propertyAddress = "216 Johnston st Abbotsford,au"; //can potentially get the element by the propertyaddress
+
     
-    var propertyAddress = "216%20Johnston%20St,%20Abbotsford%20VIC%203067,au"; //can potentially get the element by the propertyaddress
-    
-    function distanceToWork (){
         // [ORIGINAL]var queryURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=" + apiKey;
-    
-       var queryURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + propertyAddress + "&destinations=" + workAddress + "&key=" + apiKey;
-       console.log(queryURL);
-        
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response){
-            console.log(response);
+
+        var queryURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + propertyAddress + "&destinations=" + workAddress + "&key=" + apiKey;
+        console.log(queryURL);
+
+        // $.ajax({
+        //     url: queryURL,
+        //     method: "GET",
+        // }).then(function(response){
+        //     console.log(response);
+
+            //     });
+    // }
+
+    // distanceToWork()
+        var service = new google.maps.DistanceMatrixService();
+        service.getDistanceMatrix(
+            {
+                origins: [workAddress],
+                destinations: [propertyAddress],
+                travelMode: 'DRIVING',
+                // transitOptions: TransitOptions,
+                // drivingOptions: DrivingOptions,
+                // unitSystem: UnitSystem,
+                // avoidHighways: Boolean,
+                // avoidTolls: Boolean,
+            }, function (response) {
+                console.log(response)
+            })
+        }
 
 
 
 
-            
-        });
-    }
-    
-    distanceToWork()
-
-    
-});
 
 
 
