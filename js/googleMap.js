0770e1dd329fdf4dded7f7f392085b9d51d4b41f
shakib <shakib1122@gmail.com>
2020-06-27 12:59:31 +1000

    console.log resposne google

--- js/googleMap.js
+++ js/googleMap.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     apiKey ="AIzaSyDkoaZdFxBFFsVXF2Hey8DXZ-tIiR_NBYM"
 
-    var workAddress = "436 Johnston St, Abbotsford VIC 3067,au";
+    var workAddress = "436%20Johnston%20St,%20Abbotsford%20VIC%203067,au";
     
-    var propertyAddress = "216 Johnston St, Fitzroy VIC 3067,au"; //can potentially get the element by the propertyaddress
+    var propertyAddress = "216%20Johnston%20St,%20Abbotsford%20VIC%203067,au"; //can potentially get the element by the propertyaddress
     
     function distanceToWork (){
         // [ORIGINAL]var queryURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=" + apiKey;
@@ -11,7 +11,7 @@ $(document).ready(function() {
        var queryURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + propertyAddress + "&destinations=" + workAddress + "&key=" + apiKey;
        console.log(queryURL);
         
-        $ajax({
+        $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
@@ -19,8 +19,8 @@ $(document).ready(function() {
 
 
 
-            var distance = response.rows[0].elements[0].distance.value;
-            console.log("the distance is"+ distance);
+
+            
         });
     }
     
