425af037fddb53e3985298a66269a7ce136e4cc7
ben_coelho <ben_coelho@hotmail.com>
2020-06-27 11:14:30 +1000

    functional js with property price and card for output

--- js/scriptMateriallize.js
+++ js/scriptMateriallize.js
@@ -1,24 +1,9 @@
 
 //Below are the script needed for Materiallize work
 
-$(document).ready(function(){
-  $('select').formSelect();
-});
+// $(document).ready(function(){
+//   $('select').formSelect();
+// });
         
 // *********************************//
 
-
-var slider = document.getElementById('test-slider');
-noUiSlider.create(slider, {
- start: [10, 90],
- connect: true,
- step: 1,
- orientation: 'horizontal', // 'horizontal' or 'vertical'
- range: {
-   'min': 0,
-   'max': 10000
- },
- format: wNumb({
-   decimals: 0
- })
-});
