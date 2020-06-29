7c23b59e2b3b2f6e8f681888a974e74c8823346b
ben_coelho <ben_coelho@hotmail.com>
2020-06-29 21:46:45 +1000

    save and retreive work from local store

--- js/savedHistory.js
+++ js/savedHistory.js
@@ -3,33 +3,33 @@ $(document).ready(function () {
     var retrieveStorage = localStorage["favoriteStore"];
     var favoriteProps = retrieveStorage ? JSON.parse(retrieveStorage) : [];
     
-    $("#work-input").on("click", function (event) {
-   
-   
+    $("#saveWorkAddress").on("click", function (event) {
+        var myWork=$("#work-input").val()
+        localStorage["myWorkStore"] = myWork;
+        $("#table-body").empty();
+        renderFavorites();
     });
 
+var myWork=localStorage["myWorkStore"];
 
+    
+function renderFavorites(){
     for(var i=0; i<favoriteProps.length; i++){
+
 propertyAddress=favoriteProps[i].listing.propertyDetails.displayableAddress;
-console.log(propertyAddress);
-
-
 var thisRow= $("<tr></tr>");
 var thisAddress= $("<td>"+propertyAddress+"</td>");
 var thisDistance= $("<td>00</td>");
 var thisTime= $("<td>00</td>");
 
-
 $("#table-body").append(thisRow.append(thisAddress,thisDistance,thisTime))
-    }
+    };
+
+};
+
+renderFavorites();
 
 
-
-
-
-
-    console.log(favoriteProps);
-    console.log("Hello")
 
 });
 
