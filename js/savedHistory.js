9cadd0a966b5057427757b061dd1cb793f2f7689
ben_coelho <ben_coelho@hotmail.com>
2020-07-06 13:19:01 +1000

    Deleted testing files

--- js/savedHistory.js
+++ js/savedHistory.js
@@ -10,61 +10,61 @@ $(document).ready(function () {
   });
 
 
-myPropertyAddressArray=[]
-for(i=0;i<favoriteProps.length;i++){
-   myPropertyAddress =
-   favoriteProps[i].listing.propertyDetails.displayableAddress;
+  myPropertyAddressArray = []
+  for (i = 0; i < favoriteProps.length; i++) {
+    myPropertyAddress =
+      favoriteProps[i].listing.propertyDetails.displayableAddress;
 
-   myPropertyAddressArray.push(myPropertyAddress)
+    myPropertyAddressArray.push(myPropertyAddress)
 
   }
 
 
   function initMap() {
-travelDataArray=[]
-for(i=0;i<favoriteProps.length;i++){
-    apiKey = "AIzaSyDkoaZdFxBFFsVXF2Hey8DXZ-tIiR_NBYM";
+    travelDataArray = []
+    for (i = 0; i < favoriteProps.length; i++) {
+      apiKey = "AIzaSyDkoaZdFxBFFsVXF2Hey8DXZ-tIiR_NBYM";
 
-    var workAddress = myWork;
+      var workAddress = myWork;
 
-    var propertyAddress = favoriteProps[i].listing.propertyDetails.displayableAddress;
+      var propertyAddress = favoriteProps[i].listing.propertyDetails.displayableAddress;
 
-    var queryURL =
-      "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" +
-      propertyAddress +
-      "&destinations=" +
-      workAddress +
-      "&key=" +
-      apiKey;
-    console.log(queryURL);
+      var queryURL =
+        "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" +
+        propertyAddress +
+        "&destinations=" +
+        workAddress +
+        "&key=" +
+        apiKey;
+      console.log(queryURL);
 
-    var service = new google.maps.DistanceMatrixService();
-    service.getDistanceMatrix(
-      {
-        origins: [workAddress],
-        destinations: [propertyAddress],
-        travelMode: "DRIVING",
-        // transitOptions: TransitOptions,
-        // drivingOptions: DrivingOptions,
-        // unitSystem: UnitSystem,
-        // avoidHighways: Boolean,
-        // avoidTolls: Boolean,
-      },
-      function (response) {
-        var distanceToWork = response.rows[0].elements[0].distance.text;
-        var duration = response.rows[0].elements[0].duration.text;
+      var service = new google.maps.DistanceMatrixService();
+      service.getDistanceMatrix(
+        {
+          origins: [workAddress],
+          destinations: [propertyAddress],
+          travelMode: "DRIVING",
+          // transitOptions: TransitOptions,
+          // drivingOptions: DrivingOptions,
+          // unitSystem: UnitSystem,
+          // avoidHighways: Boolean,
+          // avoidTolls: Boolean,
+        },
+        function (response) {
+          var distanceToWork = response.rows[0].elements[0].distance.text;
+          var duration = response.rows[0].elements[0].duration.text;
 
-        travelData = {
-          distance: distanceToWork,
-          time: duration,
-        };
+          travelData = {
+            distance: distanceToWork,
+            time: duration,
+          };
 
-        travelDataArray.push(travelData);
-       console.log(travelDataArray);
-       $("#table-body").empty();
-  renderFavorites(travelDataArray);
-      }
-    );
+          travelDataArray.push(travelData);
+          console.log(travelDataArray);
+          $("#table-body").empty();
+          renderFavorites(travelDataArray);
+        }
+      );
     };
   };
 
@@ -76,9 +76,9 @@ for(i=0;i<favoriteProps.length;i++){
       var thisRow = $("<tr></tr>");
       var thisAddress = $("<td>" + myPropertyAddressArray[i] + "</td>");
 
-  
-      var thisDistance = $("<td>"+travelDataArray[i].distance+"</td>");
-      var thisTime = $("<td>"+travelDataArray[i].time+"</td>");
+
+      var thisDistance = $("<td>" + travelDataArray[i].distance + "</td>");
+      var thisTime = $("<td>" + travelDataArray[i].time + "</td>");
 
       $("#table-body").append(
         thisRow.append(thisAddress, thisDistance, thisTime)
