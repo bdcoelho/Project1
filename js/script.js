698dcf7bdb45cde6812cf9e31a2e924fc671fb40
bdcoelho <ben_coelho@hotmail.com>
2020-09-27 18:41:22 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
 
   function buildGeoCodeURL(searchTerm) {
     var queryURLGeo = "https://maps.googleapis.com/maps/api/geocode/json?";
-    var queryParams = { key: "AIzaSyAwmiVLmIUNhiWqaGiGzlHl7WIec1ST8Ys" };
+    var queryParams = { key: "AIzaSyDyO_UovT4ONWNjfYGnYpN_EnFBaw9Bolg" };
     queryParams.address = (searchTerm.val()+" Australia").trim();
     return queryURLGeo + $.param(queryParams);
   }
